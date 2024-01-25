92c4c10550bbe832d7b46b05ead3451c2d2c2fb9
NateBergman <nate@iphid.com>
2024-01-25 12:08:41 -0800

    got possible moves for pawns, rooks, bishops, and queens

--- src/Bishop.java
+++ src/Bishop.java
@@ -7,6 +7,50 @@ public class Bishop extends Piece {
     public ArrayList<int[]> getMoves() {
         int[] location = getPosition();
         ArrayList<int[]> moves = new ArrayList<int[]>();
+
+        int x = location[0] + 1;
+        int y = location[1] + 1;
+        while (x < 8 && y < 8 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            x += 1;
+            y += 1;
+        }
+        if (x < 8 && y < 8 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] - 1;
+        y = location[1] - 1;
+        while (y > -1 && x > -1 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            x -= 1;
+            y -= 1;
+        }
+        if (x > -1 && y > -1 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] - 1;
+        y = location[1] + 1;
+        while (x > -1 && y < 8 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            y += 1;
+            x -= 1;
+        }
+        if (x > -1 && y < 8 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] + 1;
+        y = location[1] - 1;
+        while (x < 8 && y > -1 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            y -= 1;
+            x += 1;
+        }
+        if (x < 8 && y > -1 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
         return moves;
     }
     public String toString() {
--- src/Queen.java
+++ src/Queen.java
@@ -7,6 +7,90 @@ public class Queen extends Piece {
     public ArrayList<int[]> getMoves() {
         int[] location = getPosition();
         ArrayList<int[]> moves = new ArrayList<int[]>();
+
+        int x = location[0] + 1;
+        int y = location[1] + 1;
+        while (x < 8 && y < 8 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            x += 1;
+            y += 1;
+        }
+        if (x < 8 && y < 8 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] - 1;
+        y = location[1] - 1;
+        while (y > -1 && x > -1 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            x -= 1;
+            y -= 1;
+        }
+        if (x > -1 && y > -1 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] - 1;
+        y = location[1] + 1;
+        while (x > -1 && y < 8 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            y += 1;
+            x -= 1;
+        }
+        if (x > -1 && y < 8 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] + 1;
+        y = location[1] - 1;
+        while (x < 8 && y > -1 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            y -= 1;
+            x += 1;
+        }
+        if (x < 8 && y > -1 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] + 1;
+        y = location[1];
+
+        while (x < 8 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            x += 1;
+        }
+        if (x < 8 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0] - 1;
+        while (x > -1 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            x -= 1;
+        }
+        if (x > -1 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        x = location[0];
+        y = location[1] + 1;
+        while (y < 8 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            y += 1;
+        }
+        if (y < 8 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
+        y = location[1] - 1;
+        while (y > -1 && board.atCoordinate(new int[] {x,y}) == null) {
+            moves.add(new int[] {x,y});
+            y -= 1;
+        }
+        if (y > -1 && board.atCoordinate(new int[] {x,y}) != null && board.atCoordinate(new int[] {x,y}).getWhitePiece() != white) {
+            moves.add(new int[] {x,y});
+        }
+
         return moves;
     }
     public String toString() {
