fa24f2748a2da30b805ec5d63b922eeed41f861c
NateBergman <nate@iphid.com>
2024-01-30 13:01:15 -0800

    Added completely random bot

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,13 @@ public class Main {
 
         Scanner console = new Scanner(System.in);
         boolean whiteMove = true;
+        System.out.print("Enter 1 for white player, 2 for white bot");
+        boolean whiteBot = console.nextInt() == 2;
+        System.out.print("Enter 1 for black player, 2 for black bot");
+        boolean blackBot = console.nextInt() == 2;
 
         while (true) {
             System.out.println(board + "\n");
-
             boolean stuck = true;
             ArrayList<Piece> pieces = board.getAllPiecesOfColor(whiteMove);
             for (Piece p : pieces) {
@@ -38,24 +41,31 @@ public class Main {
             }
             int[] from = new int[2];
             int[] to = new int[2];
-            boolean repeat = true;
-            do {
+            if ((whiteMove && whiteBot) || (!whiteMove && blackBot)) { //randomly picks a legal move
+                Random rand = new Random();
+                Piece p = pieces.get(rand.nextInt(pieces.size() - 1));
+                from = p.getPosition();
+                to = p.getMoves().get(rand.nextInt(p.getMoves().size() - 1));
+            } else { //get player move
+                boolean repeat = true;
                 do {
-                    System.out.print("Enter coordinates of the piece you would like to move\nX : ");
-                    from[0] = ((int)console.next().charAt(0)) - 'a';
+                    do {
+                        System.out.print("Enter coordinates of the piece you would like to move\nX : ");
+                        from[0] = ((int) console.next().charAt(0)) - 'a';
+                        System.out.print("Y : ");
+                        from[1] = console.nextInt() - 1;
+                    } while (!(from[0] > -1 && from[0] < 8 && from[1] > -1 && from[1] < 8 && board.atCoordinate(from) != null && board.atCoordinate(from).getWhitePiece() == whiteMove));
+                    System.out.print("Enter coordinates of where you would like to move it to\nX : ");
+                    to[0] = ((int) console.next().charAt(0)) - 'a';
                     System.out.print("Y : ");
-                    from[1] = console.nextInt() - 1;
-                } while (!(from[0] > -1 && from[0] < 8 && from[1] > -1 && from[1] < 8 && board.atCoordinate(from) != null && board.atCoordinate(from).getWhitePiece() == whiteMove));
-                System.out.print("Enter coordinates of where you would like to move it to\nX : ");
-                to[0] = ((int)console.next().charAt(0)) - 'a';
-                System.out.print("Y : ");
-                to[1] = console.nextInt() - 1;
-                for (int[] m : board.atCoordinate(from).getMoves()) {
-                    if (Arrays.equals(m,to)) {
-                        repeat = false;
+                    to[1] = console.nextInt() - 1;
+                    for (int[] m : board.atCoordinate(from).getMoves()) {
+                        if (Arrays.equals(m, to)) {
+                            repeat = false;
+                        }
                     }
-                }
-            } while (repeat);
+                } while (repeat);
+            }
 
             if (board.atCoordinate(from).getClass() == King.class && from[0] == 4 && to[0] == 2) {//moves rook along with king when castling
                 board.move(new int[] {0,from[1]}, new int[] {3,from[1]}, false);
